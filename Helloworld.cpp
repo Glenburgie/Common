2918805f766d32cc20fd39aa5223cfece5122e39
Glenburgie <panyanyang13@outlook.com>
2025-02-11 01:05:07 +0800

    C++ 面向对象尝鲜

--- Helloworld.cpp
+++ Helloworld.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+class cla {
+public:
+    int a,b;
+    void s(int o, int t)
+    {
+        a = o;
+        b = t;
+        cout << a+b << endl;
+    }
+};
+
+int main()
+{
     char hw[] = "Hello, world!";
     cout << hw << endl;
+    cla ne{};
+    ne.s(1,2);
 }
